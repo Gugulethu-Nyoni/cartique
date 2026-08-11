5d737de481932cf31756a8d31eeeb7f8d8508d5c
Gugu <gugunnn@gmail.com>
2026-08-11 09:47:04 +0200

    feat(wishlist): export WishlistRenderer from renderers index
    Phase 2: Wishlist Listing UI

--- storefront/src/renderers/index.js
+++ storefront/src/renderers/index.js
@@ -7,3 +7,4 @@
 export { default as ProductRenderer } from './ProductRenderer.js';
 export { default as CollectionRenderer } from './CollectionRenderer.js';
 export { default as CartRenderer } from './CartRenderer.js';
+export { default as WishlistRenderer } from './WishlistRenderer.js';
